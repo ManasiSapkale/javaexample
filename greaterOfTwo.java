f30631f95b3020a2c8ef5596021c1298f23edb29
Manasi Sapkale <124051335+ManasiSapkale@users.noreply.github.com>
2023-02-20 21:39:37 +0530

    Add files via upload

---
+++ greaterOfTwo.java
@@ -0,0 +1,27 @@
+class greaterOfTwo
+
+{
+
+public static void main(String args[])
+
+{
+
+int a=10,b=20;
+if(a>b)
+
+{
+System.out.println("a is greater");
+
+}
+
+else
+
+{
+
+System.out.println("b is greater");
+
+}
+
+}
+
+}
